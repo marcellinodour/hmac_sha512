7823a671bfec66b822a99cc8823e88507634e86c
marcellinodour <47887318+marcellinodour@users.noreply.github.com>
2022-06-27 12:07:51 +0200

    Update hmac_sha512.h

--- hmac_sha512_component/hmac_sha512.h
+++ hmac_sha512_component/hmac_sha512.h
@@ -7,22 +7,12 @@ extern "C" {
 
 #include <stddef.h>
 
-size_t  // Returns the number of bytes written to `out`
+std::string  
 hmac_sha512(
     // [in]: The key and its length.
     //      Should be at least 32 bytes long for optimal security.
-    const void* key,
-    const size_t keylen,
-
-    // [in]: The data to hash alongside the key.
-    const void* data,
-    const size_t datalen,
-
-    // [out]: The output hash.
-    //      Should be 32 bytes long. If it's less than 32 bytes,
-    //      the resulting hash will be truncated to the specified length.
-    void* out,
-    const size_t outlen);
+    const std::string key,
+    const std::string data);
 
 #ifdef __cplusplus
 }
