b8209a87f51389e54cd55fc18c118c4a6c02c655
marcellinodour <47887318+marcellinodour@users.noreply.github.com>
2022-06-27 12:10:19 +0200

    Update hmac_sha512.h

--- hmac_sha512_component/hmac_sha512.h
+++ hmac_sha512_component/hmac_sha512.h
@@ -1,10 +1,6 @@
 #ifndef _HMAC_SHA512_H_
 #define _HMAC_SHA512_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif  // __cplusplus
-
 #include <stddef.h>
 #include <string>
 
@@ -14,9 +10,5 @@ hmac_sha512(
     //      Should be at least 32 bytes long for optimal security.
     const std::string key,
     const std::string data);
-
-#ifdef __cplusplus
-}
-#endif  // __cplusplus
 
 #endif  // _HMAC_SHA512_H_
