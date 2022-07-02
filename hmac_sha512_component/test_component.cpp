7c7ce0893423f2efefe7f1af88c7351c2e3e3c8b
marcellinodour <marcellinodour@instance-1.europe-west1-b.c.m2if-348418.internal>
2022-07-02 16:38:48 +0000

    lastupdate

--- hmac_sha512_component/test_component.cpp
+++ hmac_sha512_component/test_component.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main(){
 
-    char input[2][256] = {"9b3fea674ae24cc4a0b8fd41c35d8d4ee1739d7ff9ce100ca4acf595a56a839a3df422726e48103c7d1cebe5ada6249a353d0bd81ddb5a168bbc93a06bbf85a7", "0c79bfa8d9c63b4f471484a98651012e250ba0280335ce62dd57ef63fefd242c357339067b326fdbc40bbd9de372a1dda1448127d15ca53e23f87b232ed2ba89"};
-    string output[2] = {"39a8135e0f819cc9583dabd86d7ce83948d3623b90b5523bc526ec65acbfd05271b52756045d03830589976b53b5e1a8840ffb6e505ac6f76beb75fb0fcb7fc7", "1511b152a83eb955ebc92b8b8577d40dd5ed9886021660f863d5e85c455ac4a9fcaf36fb716f3ef847e13c9c6806a741c32c128e81829323f7e121a30e7d4fae"};
+    char input[1][256] = {"9b3fea674ae24cc4a0b8fd41c35d8d4ee1739d7ff9ce100ca4acf595a56a839a3df422726e48103c7d1cebe5ada6249a353d0bd81ddb5a168bbc93a06bbf85a7"};
+    string output[1] = {"ce16a1fb0e76819983b0c163aeb38b36c2b27dedb58d8b9a8ab97e9811d9e0b5168dbd65876ca2f91f61146e27a529428a7d9178fb6d06cfda7da20a9ede6abf"};
     string key = "hello";
     bool result = true;
 
     for(int i = 0; i < sizeof(input)/sizeof(input[0]); i++){
-        if(hmac_sha512(input[i]).compare(output[i])!=0){
+        if(hmac_sha512(key, input[i]).compare(output[i])!=0){
 		result = false;
         }
     }
--- hmac_sha512_component/test_component.py
+++ hmac_sha512_component/test_component.py
@@ -1,11 +1,12 @@
 import hmac_sha512_component
 
-input = ["9b3fea674ae24cc4a0b8fd41c35d8d4ee1739d7ff9ce100ca4acf595a56a839a3df422726e48103c7d1cebe5ada6249a353d0bd81ddb5a168bbc93a06bbf85a7", "0c79bfa8d9c63b4f471484a98651012e250ba0280335ce62dd57ef63fefd242c357339067b326fdbc40bbd9de372a1dda1448127d15ca53e23f87b232ed2ba89"]
-output = ["0c79bfa8d9c63b4f471484a98651012e250ba0280335ce62dd57ef63fefd242c357339067b326fdbc40bbd9de372a1dda1448127d15ca53e23f87b232ed2ba89", "a547a4735fca6fc9a9f181908946ca32b5761587ea334a8a7d71828a6c4b0b403c633840acfc405866e90e8b269acee3fa8b1dc4f3baf31e100b17559b16fd4f"]
+input = ["9b3fea674ae24cc4a0b8fd41c35d8d4ee1739d7ff9ce100ca4acf595a56a839a3df422726e48103c7d1cebe5ada6249a353d0bd81ddb5a168bbc93a06bbf85a7"]
+key = "hello"
+output = ["ce16a1fb0e76819983b0c163aeb38b36c2b27dedb58d8b9a8ab97e9811d9e0b5168dbd65876ca2f91f61146e27a529428a7d9178fb6d06cfda7da20a9ede6abf"]
 correct = True
 
 for i in range (len(input)):
-    if output[i] != hmac_sha512_component.hmac_sha512(input[i]):
+    if output[i] != hmac_sha512_component.hmac_sha512(key, input[i]):
         correct = False
         
 print(correct)
